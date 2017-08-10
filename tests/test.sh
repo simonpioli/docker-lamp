be56233cec811ebcbc4754477627535494555de9
Simon Pioli <simon@simonpioli.com>
2017-08-10 21:08:40 +0100

    Start the process of reverting back to PHP 5.5, remove PHPMyAdmin, expose port 443

--- tests/test.sh
+++ tests/test.sh
@@ -5,8 +5,8 @@ echo "Testing mattrayner/lamp"
 echo "=> Sleeping to allow boot of containers"
 sleep 10
 
-echo "=> Testing 16.04 image"
-. 1604.sh
+# echo "=> Testing 16.04 image"
+# . 1604.sh
 
 echo "=> Testing 14.04 image"
 . 1404.sh
